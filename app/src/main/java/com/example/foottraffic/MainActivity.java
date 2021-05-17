1c464c5b9f1e3ed908ef700c26355f04e50b4054
Lily-QAQ <z5261483@unsw.edu.au>
2021-05-17 15:10:31 +1000

    get user location

--- app/src/main/java/com/example/foottraffic/MainActivity.java
+++ app/src/main/java/com/example/foottraffic/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.foottraffic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView addressTv = findViewById(R.id.addressTv);
+
+        String city = getIntent().getStringExtra("USER_CITY");
+        if (city != null) {
+            addressTv.setText(city);
+        } else {
+            addressTv.setText("Set Location");
+        }
+
+
     }
 }
