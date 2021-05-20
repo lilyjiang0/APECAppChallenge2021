cbccfb35b4ee03dbf8188629d0446a2100bfd6ee
Meilin Huang <meilin.huang@student.unsw.edu.au>
2021-05-20 17:26:12 +1000

    url changed

--- app/src/main/java/com/example/foottraffic/APIInterfaceActivity.java
+++ app/src/main/java/com/example/foottraffic/APIInterfaceActivity.java
@@ -24,6 +24,10 @@ interface APIInterfaceActivity {
     @POST("forecasts/live?api_key_private=pri_e435dec0a2aa4b8e8b4ef42bc990f596&venue_id=ven_51387131543761435650505241346a394a6432395362654a496843")
     Call<MultipleResourceActivity> doCreateInformationWithField(@Field("api_key_private") String api_key_private, @Field("venue_address") String venue_address);
 
-    @GET("venues/progress?job_id=d753cbd3-fdb6-498c-a430-59f3a036f573&collection_id=col_a9f974716aba4995a4627538b5a2cad2&ven=False")
+    // Things to do in Sydney
+    // 20 results
+    // @GET("venues/progress?job_id=d753cbd3-fdb6-498c-a430-59f3a036f573&collection_id=col_a9f974716aba4995a4627538b5a2cad2&ven=False")
+    // 1 results
+    @GET("venues/progress?job_id=fa8383a7-26f2-4ef6-97c2-ecf8111a40b5&collection_id=col_f299d036ea6f4ef584a9ccb458da2851&ven=False")
     Call<Attractions> getAttraction();
 }
